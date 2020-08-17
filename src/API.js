8a0da2a8575f347254013293d613219d288dc95e
juan <jmrada14@gmail.com>
2020-08-17 18:24:07 -0400

    wip

--- src/API.js
+++ src/API.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 const BASEURL = "";
-const APIKEY = "";
 
 export default {
-    post: (query) => axios.got(BASEURL + query + APIKEY)
+    post: (query) => axios.got(BASEURL + query)
 
 };
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Container, Row, Col, Jumbotron, Form, Button } from "react-bootstrap";
 import API from "./API";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./app.css"
+import "./app.css";
 
 function App() {
   const [response, setResponse] = useState({});
@@ -16,16 +16,63 @@ function App() {
     event.preventDefault();
     postAPI(input);
   };
+  const postJSON = {
+    FixtureData: {
+      MaxOvertimes: 0,
+      OvertimeLength: 0.08008281904610115,
+      SubCode: "NFL",
+      Code: "American",
+    },
+    MatchState: {
+      TeamInPossession: input.teamPos ? input.teamPos : "home",
+      Down: input.downVal ? input.downVal : 0,
+      CurrentQuarter: input.periodVal ? input.periodVal : 1,
+      AwayTimeoutsRemaining: input.AwayTimeRem ? input.AwayTimeRem : 3,
+      PlaySubType: "PAT",
+      Q1Score: {
+        Away: input.guestValQ1 ? input.guestValQ1 : 0,
+        Home: input.homeValQ1 ? input.homeValQ1 : 0,
+      },
+      Q2Score: {
+        Away: input.guestValQ2 ? input.guestValQ2 : 0,
+        Home: input.homeValQ2 ? input.homeValQ2 : 0,
+      },
+      Q3Score: {
+        Away: input.guestValQ3 ? input.guestValQ3 : 0,
+        Home: input.homeValQ3 ? input.homeValQ3 : 0,
+      },
+      Q4Score: {
+        Away: input.guestValQ4 ? input.guestValQ4 : 0,
+        Home: input.homeValQ4 ? input.homeValQ4 : 0,
+      },
+      OTScore: {
+        Away: input.guestValQ5 ? input.guestValQ5 : 0,
+        Home: input.homeValQ5 ? input.homeValQ5 : 0,
+      },
+      IsClockRunning: true,
+      HomeTimeoutsRemaining: input.homeTimeRem ? input.homeTimeRem : 3,
+      TeamFirstKickoff: input.firstKickOff ? input.firstKickOff : "home",
+      PlayType: "Standard",
+      YardsToGo: input.yardsToGo ? input.yardsToGo : 0,
+      YardLine: input.YardsLine ? input.YardsLine : 1,
+      TimeRemainingInQuarter: input.timeVal ? input.timeVal : 900 ,
+    },
+    TradingParameters: {
+      Supremacy: input.supremacy ? input.supremacy : 5,
+      TotalPoints: input.totalPts ? input.totalPts : 15,
+    },
+  };
+  console.log(postJSON);
   const handleInput = (event) => {
     const { target } = event;
     const value = target?.value;
-    const name = target?.name
-    setInput({...input,[name]: value});
+    const name = target?.name;
+    setInput({ ...input, [name]: value });
   };
   console.log(input);
   return (
     <Container>
-      <Jumbotron style={{marginTop: "20%"}}>
+      <Jumbotron>
         <Row>
           <Col md={3} className="border border-black">
             <div
@@ -38,7 +85,51 @@ function App() {
               <Form>
                 <Form.Group>
                   <Form.Control
-                    name="homeVal"
+                    name="homeValQ1"
+                    size="lg"
+                    type="number"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="homeValQ2"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="homeValQ3"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="homeValQ4"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="homeValQ5"
                     size="lg"
                     type="text"
                     placeholder="Large text"
@@ -75,7 +166,7 @@ function App() {
                 className="h1 border border-black "
                 style={{ margin: "0 auto" }}
               >
-                PERIOD
+                Current quarter
               </div>
               <div>
                 <Form>
@@ -91,6 +182,174 @@ function App() {
                 </Form>
               </div>
             </Row>
+            <Row>
+              <div
+                className="h1 border border-black "
+                style={{ margin: "0 auto" }}
+              >
+                Team in pos
+              </div>
+              <div>
+                <Form>
+                  <Form.Group>
+                    <Form.Control
+                      name="teamPos"
+                      size="lg"
+                      type="text"
+                      placeholder="Large text"
+                      onChange={handleInput}
+                    />
+                  </Form.Group>
+                </Form>
+                <Row>
+                  <div
+                    className="h1 border border-black "
+                    style={{ margin: "0 auto" }}
+                  >
+                    Home timeout rem
+                  </div>
+                  <div>
+                    <Form>
+                      <Form.Group>
+                        <Form.Control
+                          name="homeTimeRem"
+                          size="lg"
+                          type="text"
+                          placeholder="Large text"
+                          onChange={handleInput}
+                        />
+                      </Form.Group>
+                    </Form>
+                  </div>
+                </Row>
+                <Row>
+                  <div
+                    className="h1 border border-black "
+                    style={{ margin: "0 auto" }}
+                  >
+                    Away timeout rem
+                  </div>
+                  <div>
+                    <Form>
+                      <Form.Group>
+                        <Form.Control
+                          name="AwayTimeRem"
+                          size="lg"
+                          type="text"
+                          placeholder="Large text"
+                          onChange={handleInput}
+                        />
+                      </Form.Group>
+                    </Form>
+                  </div>
+                </Row>
+                <Row>
+                  <Col>
+                    <div>
+                      <div
+                        className="h1 border border-black "
+                        style={{ margin: "0 auto" }}
+                      >
+                        Yards to go
+                      </div>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            name="yardsToGo"
+                            size="lg"
+                            type="text"
+                            placeholder="Large text"
+                            onChange={handleInput}
+                          />
+                        </Form.Group>
+                      </Form>
+                    </div>
+                    <div>
+                      <div
+                        className="h1 border border-black "
+                        style={{ margin: "0 auto" }}
+                      >
+                        Yards Line
+                      </div>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            name="YardsLine"
+                            size="lg"
+                            type="text"
+                            placeholder="Large text"
+                            onChange={handleInput}
+                          />
+                        </Form.Group>
+                      </Form>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <div>
+                      <div
+                        className="h1 border border-black "
+                        style={{ margin: "0 auto" }}
+                      >
+                        Supremacy
+                      </div>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            name="supremacy"
+                            size="lg"
+                            type="text"
+                            placeholder="Large text"
+                            onChange={handleInput}
+                          />
+                        </Form.Group>
+                      </Form>
+                    </div>
+                    <div>
+                      <div
+                        className="h1 border border-black "
+                        style={{ margin: "0 auto" }}
+                      >
+                        total pts
+                      </div>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            name="totalPts"
+                            size="lg"
+                            type="text"
+                            placeholder="Large text"
+                            home
+                            onChange={handleInput}
+                          />
+                        </Form.Group>
+                      </Form>
+                    </div>
+                    <div>
+                      <div
+                        className="h1 border border-black "
+                        style={{ margin: "0 auto" }}
+                      >
+                        first kick off
+                      </div>
+                      <Form>
+                        <Form.Group>
+                          <Form.Control
+                            name="firstKickOff"
+                            size="lg"
+                            type="text"
+                            placeholder="Large text"
+                            home
+                            onChange={handleInput}
+                          />
+                        </Form.Group>
+                      </Form>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Row>
           </Col>
           <Col md={3} className="border border-black">
             <div
@@ -103,7 +362,51 @@ function App() {
               <Form>
                 <Form.Group>
                   <Form.Control
-                    name="guestVal"
+                    name="guestValQ1"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="guestValQ2"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="guestValQ3"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="guestValQ4"
+                    size="lg"
+                    type="text"
+                    placeholder="Large text"
+                    onChange={handleInput}
+                  />
+                </Form.Group>
+              </Form>
+              <Form>
+                <Form.Group>
+                  <Form.Control
+                    name="guestValQ5"
                     size="lg"
                     type="text"
                     placeholder="Large text"
@@ -115,7 +418,7 @@ function App() {
           </Col>
         </Row>
 
-        <Row style={{marginTop: "5%"}}>
+        <Row style={{ marginTop: "5%" }}>
           <Col md={2} className="border border-black">
             <div>
               <Form>
@@ -171,7 +474,11 @@ function App() {
             </div>
           </Col>
         </Row>
-        <Button style={{ textAlign: "center", margin: "0 auto" }} size="lg" onClick={handleSubmit}>
+        <Button
+          style={{ textAlign: "center", margin: "0 auto" }}
+          size="lg"
+          onClick={handleSubmit}
+        >
           POST
         </Button>
       </Jumbotron>
