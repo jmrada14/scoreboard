f27ce2cf4317b5391e2805741bb3f742f9a1f35a
juan <jmrada14@gmail.com>
2020-08-24 10:35:29 -0400

    done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import FormField from "./formField";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 import Axios from "axios";
-import resEx from "./data/response.json"
+import Tree from "react-json-tree"
+
 
 function App() {
   const [input, setInput] = useState({});
-  const [response, setResponse] = useState({})
+  const [response, setResponse] = useState({});
   const postAPI = (url) => {
     Axios.post(url, postJSON)
       .then((res) => setResponse(res))
@@ -18,7 +19,6 @@ function App() {
     event.preventDefault();
     postAPI("http://localhost:8080/AmericanFootballModel/s");
   };
-  console.log(resEx)
   const postJSON = {
     FixtureData: {
       MaxOvertimes: 0,
@@ -89,41 +89,46 @@ function App() {
               onChange={handleInput}
               pattern="/^\d+$/"
               type="number"
-              value={input.homeValQ1 ? input.homeValQ1 : 0 }
+              value={input.homeValQ1 ? input.homeValQ1 : 0}
             />
             <FormField
               name="homeValQ2"
               onChange={handleInput}
               pattern="/^\d+$/"
               type="number"
-              value={input.homeValQ2 ? input.homeValQ2 : 0 }
+              value={input.homeValQ2 ? input.homeValQ2 : 0}
             />
             <FormField
               name="homeValQ3"
               onChange={handleInput}
               pattern="/^\d+$/"
               type="number"
-              value={input.homeValQ3 ? input.homeValQ3 : 0 }
+              value={input.homeValQ3 ? input.homeValQ3 : 0}
             />
             <FormField
               name="homeValQ4"
               onChange={handleInput}
               pattern="/^\d+$/"
               type="number"
-              value={input.homeValQ4 ? input.homeValQ4 : 0 }
+              value={input.homeValQ4 ? input.homeValQ4 : 0}
             />
             <FormField
               name="homeValQ5"
               onChange={handleInput}
               pattern="/^\d+$/"
               type="number"
-              value={input.homeValQ5 ? input.homeValQ5 : 0 }
+              value={input.homeValQ5 ? input.homeValQ5 : 0}
             />
           </Col>
           <Col className="border border-black">
             <Row>
               <Col md="6">
-                <FormField name="timeVal" onChange={handleInput} label="TIME" value={input.timeVal ? input.timeVal : 700 }/>
+                <FormField
+                  name="timeVal"
+                  onChange={handleInput}
+                  label="TIME"
+                  value={input.timeVal ? input.timeVal : 700}
+                />
               </Col>
               <Col md="6">
                 <FormField
@@ -131,7 +136,7 @@ function App() {
                   onChange={handleInput}
                   label="CurrentQ"
                   type="number"
-                  value={input.periodVal ? input.periodVal : 1 }
+                  value={input.periodVal ? input.periodVal : 1}
                 />
               </Col>
             </Row>
@@ -149,7 +154,7 @@ function App() {
                     as="select"
                     custom
                     onChange={handleInput}
-                    value={input.teamPos ? input.teamPos : "Home" }
+                    value={input.teamPos ? input.teamPos : "Home"}
                   >
                     <option>Home</option>
                     <option>Away</option>
@@ -169,7 +174,7 @@ function App() {
                     as="select"
                     custom
                     onChange={handleInput}
-                    value={input.firstKickOff ? input.firstKickOff : 0 }
+                    value={input.firstKickOff ? input.firstKickOff : 0}
                   >
                     <option>Home</option>
                     <option>Away</option>
@@ -185,7 +190,7 @@ function App() {
                   onChange={handleInput}
                   label="Home timeout rem"
                   type="number"
-                  value={input.homeTimeRem ? input.homeTimeRem : 0 }
+                  value={input.homeTimeRem ? input.homeTimeRem : 0}
                 />
               </Col>
               <Col md="6">
@@ -194,7 +199,7 @@ function App() {
                   onChange={handleInput}
                   label="Away timeout rem"
                   type="number"
-                  value={input.AwayTimeRem ? input.AwayTimeRem : 0 }
+                  value={input.AwayTimeRem ? input.AwayTimeRem : 0}
                 />
               </Col>
             </Row>
@@ -205,7 +210,7 @@ function App() {
                   onChange={handleInput}
                   label="YardsToGo"
                   type="number"
-                  value={input.yardsToGo ? input.yardsToGo : 0 }
+                  value={input.yardsToGo ? input.yardsToGo : 0}
                 />
               </Col>
               <Col md="6">
@@ -214,7 +219,7 @@ function App() {
                   onChange={handleInput}
                   label="Yards Line"
                   type="number"
-                  value={input.YardLine ? input.YardLine : 0 }
+                  value={input.YardLine ? input.YardLine : 0}
                 />
               </Col>
             </Row>
@@ -226,7 +231,7 @@ function App() {
                     name="downVal"
                     onChange={handleInput}
                     label="DOWN"
-                    value={input.downVal ? input.downVal : 0 }
+                    value={input.downVal ? input.downVal : 0}
                   />
                 </Col>
               </Row>
@@ -236,7 +241,7 @@ function App() {
                   onChange={handleInput}
                   label="total pts"
                   type="number"
-                  value={input.totalPts ? input.totalPts : 0 }
+                  value={input.totalPts ? input.totalPts : 0}
                 />
               </Col>
               <Col md="6">
@@ -245,7 +250,7 @@ function App() {
                   onChange={handleInput}
                   label="Supremacy"
                   type="number"
-                  value={input.supremacy ? input.supremacy: 0 }
+                  value={input.supremacy ? input.supremacy : 0}
                 />
               </Col>
             </Row>
@@ -264,35 +269,35 @@ function App() {
                 onChange={handleInput}
                 pattern="/^\d+$/"
                 type="number"
-                value={input.guestValQ1 ? input.guestValQ1 : 0 }
+                value={input.guestValQ1 ? input.guestValQ1 : 0}
               />
               <FormField
                 name="guestValQ2"
                 onChange={handleInput}
                 pattern="/^\d+$/"
                 type="number"
-                value={input.guestValQ2 ? input.guestValQ2 : 0 }
+                value={input.guestValQ2 ? input.guestValQ2 : 0}
               />
               <FormField
                 name="guestValQ3"
                 onChange={handleInput}
                 pattern="/^\d+$/"
                 type="number"
-                value={input.guestValQ3 ? input.guestValQ3 : 0 }
+                value={input.guestValQ3 ? input.guestValQ3 : 0}
               />
               <FormField
                 name="guestValQ4"
                 onChange={handleInput}
                 pattern="/^\d+$/"
                 type="number"
-                value={input.guestValQ4 ? input.guestValQ4 : 0 }
+                value={input.guestValQ4 ? input.guestValQ4 : 0}
               />
               <FormField
                 name="guestValQ5"
                 onChange={handleInput}
                 pattern="/^\d+$/"
                 type="number"
-                value={input.guestValQ5 ? input.guestValQ5 : 0 }
+                value={input.guestValQ5 ? input.guestValQ5 : 0}
               />
             </div>
           </Col>
@@ -306,7 +311,9 @@ function App() {
           </Button>
         </Row>
       </Jumbotron>
-      <div>{JSON.stringify(response)}</div>
+      <div>
+        <Tree data={response} />
+      </div>
     </Container>
   );
 }
